be76e6533c71f68dda6baa7131214090eeb23d6e
vaniiiii <ivandzambas@gmail.com>
2024-04-29 19:52:50 +0200

    Added SearchBar styles 🎨

--- nextjs/components/SearchBar.tsx
+++ nextjs/components/SearchBar.tsx
@@ -1,7 +1,11 @@
 export default function SearchBar() {
   return (
     <div className="p-10 text-2xl">
-      <input type="text" placeholder="Search"></input>
+      <input
+        type="text"
+        placeholder="Search"
+        className="h-20 md:text-4xl text-3xl text-white placeholder-white placeholder-opacity-50 bg-black bg-opacity-20 outline-none border-0 rounded-full transition-all duration-200 ease-in-out appearance-none block text-center"
+      ></input>
     </div>
   );
 }
