bae24b3835735ff5c52588ac9ded40cf5e351a47
vaniiiii <ivandzambas@gmail.com>
2024-04-30 01:29:39 +0200

    VaultInfo polishing 🏓

--- nextjs/components/VaultInfo.tsx
+++ nextjs/components/VaultInfo.tsx
@@ -1,7 +1,6 @@
 import { useContractRead } from "wagmi";
 import { parseVaultInfo } from "@/utils/utils";
 import { VAULT_INFO_ABI, VAULT_INFO_ADDRESS } from "@/utils/constants";
-import { parse } from "path";
 
 export default function VaultInfo({ vaultNumber }: { vaultNumber: string }) {
   const { data, isError, isLoading } = useContractRead({
@@ -12,32 +11,53 @@ export default function VaultInfo({ vaultNumber }: { vaultNumber: string }) {
     chainId: 1,
   });
 
+  if (vaultNumber === "") return null;
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error fetching data...</p>;
 
-  const { ilk, collateral, debt } = parseVaultInfo(data);
+  let {
+    ilk,
+    collateral,
+    debt,
+    collateralizationRatio,
+    minimalCollateral,
+    maximumDebt,
+  } = parseVaultInfo(data);
+
+  if (ilk === "") {
+    ilk = "--";
+    collateral = "--";
+    debt = "--";
+    collateralizationRatio = "--";
+    minimalCollateral = "--";
+    maximumDebt = "--";
+  }
 
   return (
     <div className="bg-black bg-opacity-20 p-6 rounded-lg shadow-lg flex flex-col items-center space-y-4">
       <div className="flex justify-between w-full">
         <p className="text-white font-semibold">Collateral:</p>
-        <p className="text-white">{ilk}</p>
+        <p className="text-white">
+          {collateral} {ilk}
+        </p>
       </div>
       <div className="flex justify-between w-full">
         <p className="text-white font-semibold">Debt:</p>
-        <p className="text-white">{debt}</p>
+        <p className="text-white">{debt} DAI</p>
       </div>
       <div className="flex justify-between w-full">
         <p className="text-white font-semibold">Collateralization Ratio:</p>
-        <p className="text-white">--</p>
+        <p className="text-white">{collateralizationRatio}%</p>
       </div>
       <div className="flex justify-between w-full">
-        <p className="text-white font-semibold">Minimal Collateral:</p>
-        <p className="text-white">--</p>
+        <p className="text-white font-semibold">Min. Collateral:</p>
+        <p className="text-white">
+          {minimalCollateral} {ilk}
+        </p>
       </div>
       <div className="flex justify-between w-full">
-        <p className="text-white font-semibold">Maximum Debt:</p>
-        <p className="text-white">--</p>
+        <p className="text-white font-semibold">Max. Debt:</p>
+        <p className="text-white">{maximumDebt} DAI</p>
       </div>
     </div>
   );
--- nextjs/utils/utils.ts
+++ nextjs/utils/utils.ts
@@ -1,22 +1,56 @@
 import { formatEther } from 'viem';
+import * as C from '@/utils/constants';
 
 export const parseVaultInfo = (data: any) => {
-
-  const ilk = data ? bytesToString(data[3]) : "N/A";
-  const collateral = data ? Math.trunc(parseFloat(formatEther(data[4]))) : "N/A";
-  const debt = data ? Math.trunc(parseFloat(formatEther(data[5]))) : "N/A";
+  const ilk = data ? bytesToString(data[3]) : "";
+  const collateral = data ? parseFloat(formatEther(data[4])) : 0;
+  const debt = data ? parseFloat(formatEther(data[5])) : 0;
+  const collateralizationRatio = debt !== 0 ? (100 * collateral * C.ETH_PRICE / debt) : 0;
+  const minimalCollateral = calculateMinimalCollateral(ilk, collateral, debt).toFixed(2);
+  const maximumDebt = calculateMaximumDebt(ilk, collateral, debt).toFixed(2);
 
   return {
     ilk,
-    collateral,
-    debt,
+    collateral: collateral.toFixed(2),
+    debt: debt.toFixed(2),
+    collateralizationRatio: collateralizationRatio.toFixed(0),
+    minimalCollateral,
+    maximumDebt,
   };
 };
 
 function bytesToString(hex: string): string {
-  return Buffer.from(hex.replace(/^0x/, ''), 'hex')
-    .toString()
-    .replace(/\x00/g, '');
+  return Buffer.from(hex.replace(/^0x/, ''), 'hex').toString().replace(/\x00/g, '');
 }
 
+function calculateMinimalCollateral(ilk: string, collateral: number, debt: number): number {
+  if (debt === 0) return 0;
 
+  const debtInIlk = ilk.startsWith('ETH') ? debt / C.ETH_PRICE :
+    ilk.startsWith('WBTC') ? debt / C.BTC_PRICE : 0;
+
+  if (ilk.startsWith('ETH')) {
+    return debtInIlk * C.ETH_LIQ_RATIO;
+  }
+  if (ilk.startsWith('WBTC')) {
+    return debtInIlk * C.BTC_LIQ_RATIO;
+  }
+
+  return 0;
+}
+
+function calculateMaximumDebt(ilk: string, collateral: number, debt: number): number {
+  // assuming DAI is pegged to USD 1:1
+  if (collateral === 0) return 0;
+  const collateralInUSD = ilk.startsWith('ETH') ? collateral * C.ETH_PRICE :
+    ilk.startsWith('WBTC') ? collateral * C.BTC_PRICE : 0;
+
+  if (ilk.startsWith('ETH')) {
+    return collateralInUSD / C.ETH_LIQ_RATIO;
+  }
+  if (ilk.startsWith('WBTC')) {
+    return collateralInUSD / C.BTC_LIQ_RATIO;
+  }
+
+  return 0;
+}
