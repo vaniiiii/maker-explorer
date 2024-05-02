38310f00c15b2d831730265891048fd808aec7df
vaniiiii <ivandzambas@gmail.com>
2024-05-02 23:50:15 +0200

    Added deployment script 🗴

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,10 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.25",
-};
+  solidity: {
+    version: "0.8.25",
+    settings: {
+      evmVersion: "shanghai",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    },
+  },
+  etherscan: {
+    // npx hardhat verify --network rinkeby {contractAddress} [{constructor arguments}]
+    apiKey:
+      process.env.ETHERSCAN_API_KEY !== undefined ? process.env.ETHERSCAN_API_KEY : '',
+  },
+  sourcify: {
+    enabled: true
+  },
+  networks: {
+    ethereum: {
+      chainId: 1,
+      url: 'https://cloudflare-eth.com',
+      accounts: process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    }
+  }
+}
 
 export default config;
---
+++ hardhat/scripts/deploy.ts
@@ -0,0 +1,27 @@
+import { ethers } from 'hardhat';
+import hre from 'hardhat';
+import {
+  VaultInfoV2,
+  VaultInfoV2__factory,
+} from "../typechain-types";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log('Deploying contracts with the account:', deployer.address);
+
+  const vaultInfoV2Factory: VaultInfoV2__factory = await hre.ethers.getContractFactory('VaultInfoV2') as VaultInfoV2__factory;
+  const vaultInfoV2Deployer: VaultInfoV2 = await vaultInfoV2Factory.connect(deployer).deploy();
+
+  const vaultInfoV2 = await vaultInfoV2Deployer.waitForDeployment();
+  console.log('✅ VaultInfoV2 deployed to:', await vaultInfoV2.getAddress());
+
+  // run npx hardhat verify --network ethereum ${contractAddy} command to verify
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
